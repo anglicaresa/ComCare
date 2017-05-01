bc1eafbde62ff3167a54c76e0fa61b9c178ea6bd
abhi.ryan <abhi.ryan@anglicare.com.au>
2017-05-01 16:14:37 +0930

    Adding SQL for Admission/Discharge report

--- SQL HomeCare/HCP_Admission_Discharge.sql
+++ SQL HomeCare/HCP_Admission_Discharge.sql
@@ -6,27 +6,42 @@ use ComCareProd
 DECLARE @Start_Date AS DATETIME
 DECLARE @End_Date AS DATETIME
 DECLARE @OrgName AS Varchar(64)
-SET @Start_Date = dateadd(day,datediff(day,14,GETDATE()),0)
-SET @End_Date = dateadd(day,datediff(day,0,GETDATE()),0)
-SET @OrgName = 'Home Care Barossa Yorke Peninsula'
-PRINT @Start_Date
-PRINT @End_Date
+SET @Start_Date = dateadd(day,-30,GETDATE())
+SET @End_Date = GETDATE()
+
+SET @OrgName = 'Home Care Extended North'
+
+--SET @OrgName = 'Home Care East'
+--SET @OrgName = 'Home Care North'
+--SET @OrgName = 'Home Care South'
+--SET @OrgName = 'Home Care West'
+--SET @OrgName = 'Disabilities Children'
+
 
 -----------------------------------------------------------------------------------------------------
 -----------------------------------------------------------------------------------------------------
 
 SELECT
-	IIF (J005.[Organisation_Name] = 'Home Care Barossa Yorke Peninsula', 'Home Care Extended North', J005.[Organisation_Name]) as 'Organisation_Name'
+	IIF (J005.[Organisation_Name] = 'Home Care Barossa Yorke Peninsula', 'Home Care Extended North', J005.[Organisation_Name]) 'Organisation_Name'
 	,J001.[Client_ID]
-	,J001.[Last_Name]
-	,J001.[Preferred_Name]
-	,J006.[Description] as 'Service_Type'
-	,convert(Datetime, J002.[From_Date])as 'From_Date'
-	,convert(Datetime, J002.[To_Date]) as 'To_Date'
-	,J007.[Description] as 'Service_Delivery_Outcome'
-	,J008.[Person_ID]
-	,J008.[Preferred_Name]
-	,J008.[Last_Name]
+	,concat(J001.[Last_Name],', ',J001.[Preferred_Name]) 'Client_Name'
+	,J006.[Description] 'Service_Type'
+	,IIF(J002.To_Date is null,'Admission','Discharge')'Action'
+	,convert(Datetime, J002.[From_Date]) 'From_Date'
+	,convert(Datetime, J002.[To_Date]) 'To_Date'
+	,J007.[Description] 'Service_Delivery_Outcome'
+	,IIF
+	(
+		J003.Contact_Type = 'Service Advisor'
+		,J008.[Person_ID]
+		,Null
+	)'Advisor_ID'
+	,IIF
+	(
+		J003.Contact_Type = 'Service Advisor'
+		,Concat(J008.Last_Name,', ',J008.Preferred_Name)
+		,'Not On Record'
+	) 'Service_Advisor_Name'
 
 FROM
 (
@@ -41,10 +56,28 @@ FROM
 ) J001
 
 LEFT OUTER JOIN [dbo].[Service_Delivery] J002 ON J002.[Client_ID] = J001.[Client_ID]
-LEFT OUTER JOIN [dbo].[Personal_Contact] J003 ON J003.[Person_ID] = J001.[Client_ID]
-LEFT OUTER JOIN [dbo].[Personal_Contact_Type] J004 ON J004.[Personal_Contact_Type_Code] = J003.[Personal_Contact_Type_Code]
 
-INNER JOIN 
+Left outer join
+(
+	select
+		PC.Person_ID 'Person_ID'
+		,PC.Contact_ID 'Contact_ID'
+		,PCT.Description 'Contact_Type'
+		,ROW_NUMBER()over
+		(
+			Partition by PC.Person_ID 
+			order by 
+				case 
+					when PCT.Description = 'Service Advisor' then '1'
+					else 'z'
+				end
+		)RN
+	from [dbo].[Personal_Contact] PC 
+	Inner Join [dbo].Personal_Contact_Type PCT ON PCT.Personal_Contact_Type_Code = PC.Personal_Contact_Type_Code
+
+)J003 ON J003.Person_ID = J001.Client_ID
+
+Left outer JOIN 
 (
 	Select
 		SD.Client_ID
@@ -66,16 +99,36 @@ INNER JOIN [dbo].Person J008 ON J008.[Person_ID] = J003.[Contact_ID]
 
 WHERE
 	1=1
-	AND J002.[From_Date] BETWEEN @Start_Date AND @End_Date
-	AND J004.[Description]= N'Service Advisor'
-	AND J005.[Organisation_Name] in (@OrgName)
+	AND (
+			(cast(J002.From_Date as datetime) BETWEEN @Start_Date AND @End_Date) 
+			or 
+			(cast(J002.To_Date as datetime) BETWEEN @Start_Date AND @End_Date) 
+		)
+	and J003.RN = 1
+	and IIF (J005.[Organisation_Name] = 'Home Care Barossa Yorke Peninsula', 'Home Care Extended North', J005.[Organisation_Name]) in (@OrgName)
+
+Group by
+	IIF (J005.[Organisation_Name] = 'Home Care Barossa Yorke Peninsula', 'Home Care Extended North', J005.[Organisation_Name])
+	,J001.[Client_ID]
+	,concat(J001.[Last_Name],', ',J001.[Preferred_Name])
+	,J006.[Description]
+	,convert(Datetime, J002.[From_Date])
+	,convert(Datetime, J002.[To_Date])
+	,J007.[Description]
+	,IIF
+	(
+		J003.Contact_Type = 'Service Advisor'
+		,J008.[Person_ID]
+		,Null
+	)
+	,IIF
+	(
+		J003.Contact_Type = 'Service Advisor'
+		,Concat(J008.Last_Name,', ',J008.Preferred_Name)
+		,'Not On Record'
+	)
 
 ORDER BY
-	J005.[Organisation_Name]
-	,J008.[Last_Name]
-	,J008.[Preferred_Name]
-	,J008.[Person_ID]
-	,J001.[Last_Name]
-	,J001.[Preferred_Name]
-	,J001.[Client_ID]
+	IIF (J005.[Organisation_Name] = 'Home Care Barossa Yorke Peninsula', 'Home Care Extended North', J005.[Organisation_Name])
+	,concat(J001.[Last_Name],', ',J001.[Preferred_Name])
 	,J002.[From_Date]
