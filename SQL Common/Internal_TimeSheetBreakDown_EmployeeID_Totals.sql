3e64cc1161cb172706d171e0641182460a3f21e4
abhi.ryan <abhi.ryan@anglicare.com.au>
2017-08-11 09:02:55 +0930

    adding Internal_TimeSheetBreakDown_[Totals skew]

---
+++ SQL Common/Internal_TimeSheetBreakDown_EmployeeID_Totals.sql
@@ -0,0 +1,18 @@
+declare @Start_Date date = cast('2017-05-15' as date)
+declare @End_Date date = cast('2017-05-15' as date)
+DECLARE @OrgName AS Varchar(64) = 'Home Care north'
+
+Select
+	J007.Employee_No
+from dbo.Organisation J001
+inner join dbo.Provider_Contract J006 on J006.Organisation_ID = J001.Organisation_ID
+inner join dbo.Provider J007 on J007.Provider_ID = J006.Provider_ID
+
+where
+	J001.Organisation_Name in (@OrgName)
+	and J007.Employee_No is not null
+	and 
+	(
+		cast(J006.Effective_Date_To as date) > @Start_Date
+		or J006.Effective_Date_To is null
+	)
---
+++ SQL Common/Internal_TimeSheetBreakDown_T1_totals.sql
@@ -0,0 +1,37 @@
+--select * from [az-sqlbi01].dwhb.extract.hrtim_tsheet_entry T1Ext where T1Ext.EMPLOYEE_ID = 3837 and TimeSheet_Date = cast('2017-05-15' as datetime)
+
+--/*
+declare @Start_Date date = cast('2017-07-01' as date)
+declare @End_Date date = cast('2017-07-30' as date)
+--*/
+
+
+/*
+declare @Start_Date date = cast('2017-06-05' as date)
+declare @End_Date date = cast('2017-06-05' as date)
+--*/
+
+Select 
+	distinct
+	T1.EMPLOYEE_ID
+	,Sum(T1.UNITS)over(partition by T1.EMPLOYEE_ID) 'Units'
+
+from
+(
+	select 
+		T1Ext.EMPLOYEE_ID
+		,cast(T1Ext.TIMESHEET_DATE as Date) TIMESHEET_DATE
+		,T1Ext.UNITS
+	from [az-sqlbi01].dwhb.extract.hrtim_tsheet_entry T1Ext
+	where
+		1=1
+		and cast(T1Ext.TIMESHEET_DATE as date) between @Start_Date and @End_Date
+		and T1Ext.ENTRY_TYPE = 'TW'
+		and 1 = iif(T1Ext.CLOCK_IN = 0 and T1Ext.CLOCK_OUT = 0, 0,1)
+		and T1Ext.UNIT_TYPE = 'H'
+--		and T1Ext.EMPLOYEE_ID in (@Employee_ID)
+
+		and T1Ext.EMPLOYEE_ID = 3837
+)T1
+
+order by 1, 2
