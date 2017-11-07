d75002f2674751922ff8696a46dc12606a027d75
abhi.ryan <abhi.ryan@anglicare.com.au>
2017-11-07 12:43:37 +1030

    HCP_Package_Remediation - Cancellation FIX + efficiency pass

--- SQL HomeCare/HCP_Package_Remediation.sql
+++ SQL HomeCare/HCP_Package_Remediation.sql
@@ -19,12 +19,13 @@ select * from dbo.FC_Funder_Contract --Funder_Contract_ID
 
 use ComCareProd
 
-Declare @Client_ID_ INT = 10004304
-DECLARE @StartDate Date = '2017-09-07'
-DECLARE @EndDate Date = '2017-09-20'
+Declare @Client_ID_ INT = 10014200
+DECLARE @StartDate Date = '2017-09-15'
+DECLARE @EndDate Date = '2017-09-15'
 --print @StartDate
 --Declare @Organisation Varchar(128) = 'Home Care West'
-Declare @Organisation Varchar(128) = 'Home Care North'
+--Declare @Organisation Varchar(128) = 'Home Care North'
+Declare @Organisation Varchar(128) = 'Home Care East'
 
 Declare @ContractType Table (ContractType varchar(64))
 Insert INTO @ContractType
@@ -42,10 +43,10 @@ declare @GroupActivity varchar(128) = 'HideGroupActivity'
 
 ---------------------------------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------------------------
---/*
+-->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 select * from
 (
-	select
+	select distinct
 		J001.Client_ID 'Client_ID'
 		,J001.Provider_ID 'Provider_ID'
 		,IIF(J033.WiA_Schedule_TimeKILL = 'true', null, Cast (J033.Schedule_Time as Datetime)) 'Schedule_Visit_Time'	
@@ -54,17 +55,17 @@ select * from
 		,J001.Visit_Duration 'Actual_Duration'
 		,J088.CareModel 'CareModel'
 		,J004.Description 'task_Description'
-		,iif(cast(J001.Client_Not_Home as int) = 1 or J001.Visit_Cancel_Reason_ID = 1,1,0)'Client_Not_Home'
+		,J014.Description 'Client_Not_Home'
 		,IIF (J002.Client_ID IS NULL, 0, 1) 'Has_Charge_Item'
 		,convert (int ,'0') 'In_WiA_Only'
 		,J002.Line_Description 'Charge_Item_Line_Description'
 		,J002.Amount 'Amount'
-		,J088.funderContract 'funderContract' --J088
+		,J088.funderContract 'funderContract'
 		,iif(J001.Group_Activity_ID is Null, 0, 1) 'HasGroupActivity'
 		,J001.Travel_Km 'Travel_Km'
 
 	from (select * from dbo.Actual_Service _AS where cast(_AS.Visit_Date as date) between @StartDate and @EndDate) J001
-
+	
 	Left outer Join
 	(
 		select
@@ -78,51 +79,47 @@ select * from
 			,FC_FC.Description 'funderContract'
 			,FC_FCM.Description 'CareModel'
 		from dbo.FC_Client_Contract FC_CC
-		Left outer Join dbo.FC_Funder_Contract FC_FC	on FC_FC.funder_Contract_ID = FC_CC.funder_Contract_ID
+		Left outer Join dbo.FC_Funder_Contract FC_FC on FC_FC.funder_Contract_ID = FC_CC.funder_Contract_ID
 		Left outer Join dbo.FC_Funding_Care_Model FC_FCM on FC_FCM.Funding_Care_Model_ID = FC_FC.Funding_Care_Model_ID
 		Left outer join dbo.FC_Account FC_A on FC_A.client_Contract_ID = FC_CC.client_Contract_ID
 
 		Left outer join 
 		(
 			select
-				FC_T.FC_Account_ID 'FC_Account_ID'
-				,FC_T.FC_Transaction_Type_ID 'FC_Transaction_Type_ID'
-				,FC_T.Activity_Date 'Visit_Date'
-				,FC_T.Transaction_Source 'Transaction_Source'
-				,FC_T.Estimated_Amount 'Estimated_Amount'
-				,FC_T.Actual_Amount 'Actual_Amount'
-				,FC_T.Balance_After_Txn 'Balance_After_Txn'
-				,FC_T.Source_Table 'Source_Table'
-				,FC_T.Source_Record_Key 'Source_Record_Key'
-				,FC_T.Comments 'Comments'
-				,ROW_NUMBER() over(
-									partition by FC_T.FC_Account_ID, FC_T.Activity_Date
-									order by 
-									case
-									when FC_T.Actual_Amount = 0.0 then 'zzzz'
-									else 
-									FC_T.Actual_Amount
-									end desc
-								)'RN'
-			from dbo.FC_Transaction FC_T
+				FC_T_SUB.FC_Account_ID 'FC_Account_ID'
+				,FC_T_SUB.FC_Transaction_Type_ID 'FC_Transaction_Type_ID'
+				,FC_T_SUB.Activity_Date 'Visit_Date'
+				,FC_T_SUB.Transaction_Source 'Transaction_Source'
+				,FC_T_SUB.Estimated_Amount 'Estimated_Amount'
+				,FC_T_SUB.Actual_Amount 'Actual_Amount'
+				,FC_T_SUB.Balance_After_Txn 'Balance_After_Txn'
+				,FC_T_SUB.Source_Table 'Source_Table'
+				,FC_T_SUB.Source_Record_Key 'Source_Record_Key'
+				,FC_T_SUB.Comments 'Comments'
+				,ROW_NUMBER() over
+				(
+					partition by FC_T_SUB.FC_Account_ID, FC_T_SUB.Activity_Date
+					order by case
+					when FC_T_SUB.Actual_Amount = 0.0 then 'zzzz'
+					else FC_T_SUB.Actual_Amount
+					end desc
+				)'RN' --forces results to be ordered
+			from dbo.FC_Transaction FC_T_SUB
 			where 
 				1=1
-				and FC_T.FC_Transaction_Type_ID = 3
-			--	and FC_T.Actual_Amount <> 0.0
-
+				and FC_T_SUB.FC_Transaction_Type_ID = 3
+				and FC_T_SUB.Activity_Date between @StartDate and @EndDate
 		)FC_T on FC_T.FC_Account_ID = FC_A.FC_Account_ID
 
-	)J002 ON 
-			J002.Client_ID = J001.Client_ID 
+	)J002 ON J002.Client_ID = J001.Client_ID 
 			and J002.Visit_Date = J001.Visit_Date 
 			and J002.Visit_No = J001.Visit_No
 			and J002.Service_Prov_Position_ID = J001.Service_Prov_Position_ID
 			and J002.Provider_ID = J001.Provider_ID
 
-
 	left outer join
 	(
-		select
+		select 
 			Wi_A.Client_ID
 			,Wi_A.Wi_Record_ID 'Wi_Record_ID'
 			,Wi_A.Activity_Date
@@ -134,7 +131,7 @@ select * from
 			,iif(Wi_A.ReSchedule is not null and Wi_A.Activity_Start_time is not null, 'True', 'Flase') 'WiA_Schedule_TimeKILL'
 			,AWT.Allocated_Task_ID 'Allocated_Task_ID'
 			,Wi_A.Round_Allocation_ID 'Round_Allocation_ID'
-		from dbo.wi_activity Wi_A
+		from (select * from dbo.wi_activity where Activity_Date between @StartDate and @EndDate) Wi_A
 		left outer join dbo.activity_work_table AWT on AWT.Allocated_Task_ID = Wi_A.Round_Allocation_ID and AWT.activity_date = Wi_A.activity_date
 	)J033 ON 
 		J033.Client_ID = J001.Client_ID 
@@ -144,102 +141,61 @@ select * from
 	
 	Left outer Join
 	(
-		select
+		select distinct
 			TT.Task_Type_Code
+			,TT.Service_Type_Code
 			,SD.Client_ID 'Client_ID'
 			,FC_FC.Description 'funderContract'
 			,FC_FCM.Description 'CareModel'
+			,SD.From_Date
+			,iif(SD.To_Date is null,cast('2100-01-01'as date),SD.To_Date)'To_Date'
 		from dbo.Task_Type TT
 		Left outer join dbo.Service_Delivery SD on SD.Service_Type_Code = TT.Service_Type_Code
 		left outer join dbo.FC_Contract_Area_Product CAP on CAP.Funding_Prog_Code = SD.Funding_Prog_Code
 		Left outer Join dbo.FC_Funder_Contract FC_FC on FC_FC.funder_Contract_ID = CAP.funder_Contract_ID
 		Left outer Join dbo.FC_Funding_Care_Model FC_FCM on FC_FCM.Funding_Care_Model_ID = FC_FC.Funding_Care_Model_ID
-
-	)J088 on J088.Client_ID = J001.Client_ID and J088.Task_Type_Code = J001.Task_Type_Code
-
+	)J088 on J088.Client_ID = J001.Client_ID and J088.Task_Type_Code = J001.Task_Type_Code and J001.Visit_Date between J088.From_Date and J088.To_Date
 
 	left outer join dbo.Task_Type J004 on J004.Task_Type_Code = J001.Task_Type_Code
-	left outer join dbo.Service_Delivery J005 ON J001.Client_ID = J005.Client_ID
-	--funding_Prog_code
+
 	left outer join 
 	(
-		Select 
+		Select distinct
 			SD.Client_ID
 			,O.Organisation_Name
 			,SD.Service_Type_Code
-			,ROW_NUMBER ()
-				over 
-				(
-					Partition by SD.Client_ID Order by
-						CASE
-						WHEN O.Organisation_Name = @Organisation THEN '1'
-						ELSE O.Organisation_Name END ASC
-				)'RN'
 		from dbo.Service_Delivery SD
 			join dbo.Period_of_Residency PR on PR.Person_ID = SD.Client_ID
 			join dbo.Address A on A.Address_ID = PR.Address_ID
 			Join dbo.Service_Provision SP on A.Suburb_ID = SP.Suburb_ID and SP.Service_Type_Code = SD.Service_Type_Code
 			Join dbo.Organisation O on Sp.Centre_ID = O.Organisation_ID
-		Where PR.To_Date is null and PR.Display_Indicator  = 1
-	) J006 ON J006.Client_ID = J001.Client_ID AND J006.Service_Type_Code = J005.Service_Type_Code
+		Where PR.Display_Indicator = 1
+	) J006 ON J006.Client_ID = J001.Client_ID and J006.Service_Type_Code = J004.Service_Type_Code
 
-	left outer Join
-	(
-		select
-			CCB.Client_ID 'Client_ID'
-			,Org.Organisation_Name 'Organisation_Name'
-			,CBG.Description 'ContractBillingGroup'
-			,CCB.Contract_Billing_ID 'Contract_Billing_ID'
-			,ROW_NUMBER ()
-				over 
-				(
-					Partition by CCB.Client_ID Order by
-						CASE
-						WHEN Org.Organisation_Name = @Organisation THEN '1'
-						ELSE Org.Organisation_Name END ASC
-				) 'RN'
-		from dbo.FB_Client_Contract_Billing CCB
-			left outer join dbo.FB_Contract_Billing_Group CBG on CBG.Contract_Billing_Group_ID = CCB.Contract_Billing_Group_ID
-			left outer Join dbo.FB_Client_Contract_Billed_To CCBT on CCBT.Client_CB_ID = CCB.Client_CB_ID
-			left outer Join dbo.FB_Client_CB_Split CCBS on CCBS.Client_Contract_Billed_To_ID = CCBT.Client_Contract_Billed_To_ID
-			left outer Join dbo.Organisation Org on CCBS.Organisation_ID = Org.Organisation_ID
-
-	)J009 on J009.Client_ID = J001.Client_ID
 
 	left outer join dbo.FC_Contract_Area_Product J010 ON J010.CAP_ID = J001.CAP_ID
 	left outer join dbo.FC_Funder_Contract J011 ON J011.Funder_Contract_ID = J010.Funder_Contract_ID
+	Left outer join dbo.Visit_Cancel_Reason J014 on J014.Visit_Cancel_Reason_ID = J001.Visit_Cancel_Reason_ID
 
 	Where 
 		1=1
-		and J001.Client_ID = @Client_ID_
-		and (J006.RN < 2 or J006.RN is NULL)
-		and (J009.RN < 2 or J009.rn is null)
+--		and J001.Client_ID = @Client_ID_
 		and cast(J001.Visit_Date as date) between @StartDate and @EndDate
 		and J088.CareModel = 'Home Care Package'
-
 		and J006.Organisation_Name = @Organisation
-		AND (IIF (J088.funderContract is NULL,'No Contract',J088.funderContract) in (select * from @ContractType))
+		and (IIF (J088.funderContract is NULL,'No Contract',J088.funderContract) in (select * from @ContractType))
 --		and (IIF (J088.funderContract is NULL,'No Contract',J088.funderContract) in (@ContractType))
 
 ) t1
 
---group Activity filter
-
 ---------------------------------------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------------------------------
----------------------------------------------------------------------------------------------------------------------------------
----------------------------------------------------------------------------------------------------------------------------------
---*/
---/*
 Union
-
 ---------------------------------------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------------------------------
----------------------------------------------------------------------------------------------------------------------------------
---/*
 select * from
 (
 	select
@@ -251,12 +207,12 @@ select * from
 		,J001.AcS_Visit_Duration 'Actual_Duration'
 		,J088.CareModel 'CareModel'
 		,J004.Description 'task_Description'
-		,iif(cast(J001.Client_Not_Home as int) = 1 or J001.Visit_Cancel_Reason_ID = 1,1,0)'Client_Not_Home'
+		,J014.Description 'Client_Not_Home'
 		,IIF (J002.Client_ID IS NULL, 0, 1) 'Has_Charge_Item'
 		,IIF (J001.Client_ID IS NULL, 1, 0) 'In_WiA_Only'
 		,J002.Line_Description 'Charge_Item_Line_Description'
 		,J002.Amount 'Amount'
-		,J088.funderContract 'funderContract'--J088
+		,J088.funderContract 'funderContract'
 		,iif(J001.Group_Activity_ID is Null, 0, 1) 'HasGroupActivity'
 		,J001.Travel_Km 'Travel_Km'
 	FROM 
@@ -294,8 +250,7 @@ select * from
 			,Wi_A.Absence_Code
 			,Wi_A.Group_Activity_ID 'Group_Activity_ID'
 			,AC_S.Travel_Km 'Travel_Km'
-			,ROW_NUMBER () -- sort by importance of 'covered' 'absent' and 'Un-Alocated'.
-			over 
+			,ROW_NUMBER() over -- sort by importance of 'covered' 'absent' and 'Un-Alocated'.
 			(
 				Partition by Wi_A.Client_ID, Wi_A.Activity_Date, Wi_A.SPPID, Wi_A.Schedule_Time
 				 Order by 
@@ -306,21 +261,20 @@ select * from
 				end
 			) AS 'RN'
 		from (select * from dbo.WI_Activity where cast(Activity_Date as date) between @StartDate and @EndDate) Wi_A
-		Left Outer Join dbo.Actual_Service Ac_S 
+		Left outer join dbo.Actual_Service Ac_S 
 		ON 
 			1=1
 			and Wi_A.Client_ID = Ac_S.Client_ID 
 			and Wi_A.SPPID = Ac_S.Service_Prov_Position_ID
 			and Wi_A.Activity_Date = Ac_S.Visit_Date
 			and Wi_A.Round_Allocation_ID = Ac_S.Allocated_Task_ID
-
 		where
 			1=1
 			and Wi_A.Cancellation_Date is NULL
 			and Wi_A.Client_ID IS NOT NULL		
 	)J001
 
-	Left outer Join
+	Left outer join
 	(
 		select
 			FC_CC.Client_ID 'Client_ID'
@@ -336,36 +290,33 @@ select * from
 		Left outer Join dbo.FC_Funder_Contract FC_FC	on FC_FC.funder_Contract_ID = FC_CC.funder_Contract_ID
 		Left outer Join dbo.FC_Funding_Care_Model FC_FCM on FC_FCM.Funding_Care_Model_ID = FC_FC.Funding_Care_Model_ID
 		Left outer join dbo.FC_Account FC_A on FC_A.client_Contract_ID = FC_CC.client_Contract_ID
-		
 
 		Left outer join 
 		(
 			select 
-				FC_T.FC_Account_ID 'FC_Account_ID'
-				,FC_T.FC_Transaction_Type_ID 'FC_Transaction_Type_ID'
-				,FC_T.Activity_Date 'Visit_Date'
-				,FC_T.Transaction_Source 'Transaction_Source'
-				,FC_T.Estimated_Amount 'Estimated_Amount'
-				,FC_T.Actual_Amount 'Actual_Amount'
-				,FC_T.Balance_After_Txn 'Balance_After_Txn'
-				,FC_T.Source_Table 'Source_Table'
-				,FC_T.Source_Record_Key 'Source_Record_Key'
-				,FC_T.Comments 'Comments'
-				,ROW_NUMBER() over(
-									partition by FC_T.FC_Account_ID, FC_T.Activity_Date
-									order by 
-									case
-									when FC_T.Actual_Amount = 0.0 then 'zzzz'
-									else 
-									FC_T.Actual_Amount --desc
-									end desc
-								)'RN'
-			from dbo.FC_Transaction FC_T
+				FC_T_SUB.FC_Account_ID 'FC_Account_ID'
+				,FC_T_SUB.FC_Transaction_Type_ID 'FC_Transaction_Type_ID'
+				,FC_T_SUB.Activity_Date 'Visit_Date'
+				,FC_T_SUB.Transaction_Source 'Transaction_Source'
+				,FC_T_SUB.Estimated_Amount 'Estimated_Amount'
+				,FC_T_SUB.Actual_Amount 'Actual_Amount'
+				,FC_T_SUB.Balance_After_Txn 'Balance_After_Txn'
+				,FC_T_SUB.Source_Table 'Source_Table'
+				,FC_T_SUB.Source_Record_Key 'Source_Record_Key'
+				,FC_T_SUB.Comments 'Comments'
+				,ROW_NUMBER() over
+				(
+					partition by FC_T_SUB.FC_Account_ID, FC_T_SUB.Activity_Date
+					order by case
+					when FC_T_SUB.Actual_Amount = 0.0 then 'zzzz'
+					else FC_T_SUB.Actual_Amount
+					end desc
+				)'RN' --forces results to be ordered
+			from dbo.FC_Transaction FC_T_SUB
 			where 
 				1=1
-				and FC_T.FC_Transaction_Type_ID = 3
-			--	and FC_T.Actual_Amount <> 0.0
-
+				and FC_T_SUB.FC_Transaction_Type_ID = 3
+				and FC_T_SUB.Activity_Date between @StartDate and @EndDate
 		)FC_T on FC_T.FC_Account_ID = FC_A.FC_Account_ID
 
 	)J002 ON 
@@ -377,88 +328,52 @@ select * from
 
 	Left outer Join
 	(
-		select
+		select distinct
 			TT.Task_Type_Code
 			,SD.Client_ID 'Client_ID'
 			,FC_FC.Description 'funderContract'
 			,FC_FCM.Description 'CareModel'
+			,SD.From_Date
+			,iif(SD.To_Date is null,cast('2100-01-01'as date),SD.To_Date)'To_Date'
 		from dbo.Task_Type TT
 		Left outer join dbo.Service_Delivery SD on SD.Service_Type_Code = TT.Service_Type_Code
 		left outer join dbo.FC_Contract_Area_Product CAP on CAP.Funding_Prog_Code = SD.Funding_Prog_Code
 		Left outer Join dbo.FC_Funder_Contract FC_FC on FC_FC.funder_Contract_ID = CAP.funder_Contract_ID
 		Left outer Join dbo.FC_Funding_Care_Model FC_FCM on FC_FCM.Funding_Care_Model_ID = FC_FC.Funding_Care_Model_ID
 
-	)J088 on J088.Client_ID = J001.Client_ID and J088.Task_Type_Code = J001.Task_Type_Code
+	)J088 on J088.Client_ID = J001.Client_ID and J088.Task_Type_Code = J001.Task_Type_Code and J001.WiA_Schedule_Time between J088.From_Date and J088.To_Date
 
-	Left outer Join dbo.Task_Type J004 on J004.Task_Type_Code = J001.Task_Type_Code
-	Left Outer Join dbo.Service_Delivery J005 ON J001.Client_ID = J005.Client_ID
+	Left outer join dbo.Task_Type J004 on J004.Task_Type_Code = J001.Task_Type_Code
 
-	Left outer JOIN 
+	left outer join 
 	(
-		Select 
+		Select distinct
 			SD.Client_ID
 			,O.Organisation_Name
 			,SD.Service_Type_Code
-			,ROW_NUMBER ()
-				over 
-				(
-					Partition by SD.Client_ID Order by
-						CASE
-						WHEN O.Organisation_Name = @Organisation THEN '1'
-						ELSE O.Organisation_Name END ASC
-				) AS 'RN'
 		from dbo.Service_Delivery SD
 			join dbo.Period_of_Residency PR on PR.Person_ID = SD.Client_ID
 			join dbo.Address A on A.Address_ID = PR.Address_ID
 			Join dbo.Service_Provision SP on A.Suburb_ID = SP.Suburb_ID and SP.Service_Type_Code = SD.Service_Type_Code
 			Join dbo.Organisation O on Sp.Centre_ID = O.Organisation_ID
-		Where PR.To_Date is null and PR.Display_Indicator  = 1
-	) J006 ON J006.Client_ID = J001.Client_ID AND J006.Service_Type_Code = J005.Service_Type_Code
+		Where PR.Display_Indicator = 1
+	) J006 ON J006.Client_ID = J001.Client_ID and J006.Service_Type_Code = J004.Service_Type_Code
 
-	Left outer Join
-	(
-		select
-			CCB.Client_ID 'Client_ID'
-			,Org.Organisation_Name 'Organisation_Name'
-			,CBG.Description 'ContractBillingGroup'
-			,ROW_NUMBER ()
-				over 
-				(
-					Partition by CCB.Client_ID Order by
-						CASE
-						WHEN Org.Organisation_Name = @Organisation THEN '1'
-						ELSE Org.Organisation_Name END ASC
-				) 'RN'
-		from dbo.FB_Client_Contract_Billing CCB
-			left outer join dbo.FB_Contract_Billing_Group CBG on CBG.Contract_Billing_Group_ID = CCB.Contract_Billing_Group_ID
-			left outer Join dbo.FB_Client_Contract_Billed_To CCBT on CCBT.Client_CB_ID = CCB.Client_CB_ID
-			left outer Join dbo.FB_Client_CB_Split CCBS on CCBS.Client_Contract_Billed_To_ID = CCBT.Client_Contract_Billed_To_ID
-			left outer Join dbo.Organisation Org on CCBS.Organisation_ID = Org.Organisation_ID
-
-	)J009 on J009.Client_ID = J001.Client_ID
-
-	Left Outer Join dbo.FC_Contract_Area_Product J010 ON J010.CAP_ID = J001.CAP_ID
-	LEFT OUTER JOIN dbo.FC_Funder_Contract J011 ON J011.Funder_Contract_ID = J010.Funder_Contract_ID
+	Left outer join dbo.FC_Contract_Area_Product J010 ON J010.CAP_ID = J001.CAP_ID
+	Left outer join dbo.FC_Funder_Contract J011 ON J011.Funder_Contract_ID = J010.Funder_Contract_ID
+	Left outer join dbo.Visit_Cancel_Reason J014 on J014.Visit_Cancel_Reason_ID = J001.Visit_Cancel_Reason_ID
 
 	Where 
 		1=1
-		and J001.Client_ID = @Client_ID_
-		
+--		and J001.Client_ID = @Client_ID_	
 		and 1 = iif(J001.RN > 1 and J001.WiA_Provider_ID = 0, 0, 1)
-		and (J006.RN < 2 or J006.RN is null)
-		and (J009.RN < 2 or J009.RN is null)
 		and cast(J001.WiA_Schedule_Time as Date) between @StartDate and @EndDate
 		and J001.Client_ID IS NOT NULL
 		and J088.CareModel = 'Home Care Package'
-
 		and J006.Organisation_Name = @Organisation
 		AND (IIF (J088.funderContract is NULL,'No Contract',J088.funderContract) in (select * from @ContractType))
 --		and (IIF (J011.Description is NULL,'No Contract',J011.Description) in (@ContractType))
 
 )t2
-
-
---*/
---*/
 order by
 1,3,5,12
